d74e61b3d4145f932233b282c1a83c186c39a3f9
bilqiskz <mamimotoy@gmail.com>
2020-04-15 10:05:28 +0700

    hai

--- js/custom.js
+++ js/custom.js
@@ -58,24 +58,24 @@
             CAROUSEL 
         =========================*/
 		
-		$(document).ready(function() {
-		  $("#owl-demo").owlCarousel({
-			  autoPlay: 3000, //Set AutoPlay to 3 seconds
-			  items : 4,
-			  itemsDesktop : [1199,3],
-			  itemsDesktopSmall : [979,2]
+		// $(document).ready(function() {
+		//   $("#owl-demo").owlCarousel({
+		// 	  autoPlay: 3000, //Set AutoPlay to 3 seconds
+		// 	  items : 4,
+		// 	  itemsDesktop : [1199,3],
+		// 	  itemsDesktopSmall : [979,2]
 		 
-		  });
-		});
-		// testimoni
-		$(document).ready(function() {
-		  $(".owl-carousel2").owlCarousel({
-			  items : 3,
-			  itemsDesktop : [1199,3],
-			  itemsDesktopSmall : [979,2]
+		//   });
+		// });
+		// // testimoni
+		// $(document).ready(function() {
+		//   $(".owl-carousel2").owlCarousel({
+		// 	  items : 3,
+		// 	  itemsDesktop : [1199,3],
+		// 	  itemsDesktopSmall : [979,2]
 		 
-		  });
-		});
+		//   });
+		// });
 		/* ========================
 			SLIDER - TAB 
 		=============================*/
